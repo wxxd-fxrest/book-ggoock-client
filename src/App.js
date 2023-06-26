1118d9a49dba7df761de3c6aba2f31f44d8004ae
wxxd-fxrest <fxrest704@gmail.com>
2023-06-26 19:02:46 +0900

    book 추천 프로젝트 시작

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [backData, setBackData] = useState([{}]);
+  useEffect(() => {
+    fetch('/api').then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackData(data);
+      }
+    )
+  }, []); 
+
+  const getMusic = async() => {
+    const response = await fetch(`
+      https://www.googleapis.com/books/v1/volumes?q=꽃+&key=${process.env.REACT_APP_BOOK_API}
+    `) ;
+    const response2 = await fetch(`
+      https://www.googleapis.com/books/v1/volumes?q=안녕+&key=${process.env.REACT_APP_BOOK_API}
+    `) ;
+    const json = await response.json(); 
+    const json2 = await response2.json(); 
+    console.log(json, json2) ;
+  } ; 
+
+  useEffect(() => {
+    getMusic(); 
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {(typeof backData.user === 'undefined') ? (
+        <p> Loading... </p>
+      ) : (
+        backData.user.map((user, i) => (
+          <p key={i}> {user} </p>
+        ))
+      )}
     </div>
   );
 }
